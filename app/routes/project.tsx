a48a5681e8c73c44b628b72bda59f5978a3eb26c
Andrew Lavrentev <andrew.lavrentev@gmail.com>
2022-11-06 21:06:53 +0100

    scroll toastblock

--- app/routes/project.tsx
+++ app/routes/project.tsx
@@ -9,27 +9,27 @@ export const loader: LoaderFunction = (): LoaderData => {
 
 export default function () {
   return (
-    <main className="flex min-h-full flex-col bg-white">
-      <ProjectHeadingImage />
-      <div className="rb-content-container relative -mt-6 rounded-t-3xl bg-white pt-6">
-        <div className="px-1 pb-6">
-          <ProjectHeading />
-        </div>
+    <main className="flex h-full flex-col bg-white">
+      <div className="overflow-scroll pb-6">
+        <ProjectHeadingImage />
+        <div className="rb-content-container relative -mt-6 rounded-t-3xl bg-white pt-6">
+          <div className="px-1 pb-6">
+            <ProjectHeading />
+          </div>
 
-        <div className="-mx-5 box-border flex justify-between overflow-scroll px-4">
-          <ProjectSummarySlider />
-        </div>
+          <div className="-mx-5 box-border flex justify-between overflow-scroll px-4">
+            <ProjectSummarySlider />
+          </div>
 
-        <div className="mt-16">
-          <ProjectDetailed />
+          <div className="mt-16">
+            <ProjectDetailed />
+          </div>
         </div>
       </div>
 
-      <div className="mt-6">
-        <ToastBlock>
-          <button className="btn-md btn-primary w-full">Позвонить</button>
-        </ToastBlock>
-      </div>
+      <ToastBlock>
+        <button className="btn-md btn-primary w-full">Позвонить</button>
+      </ToastBlock>
     </main>
   );
 }
