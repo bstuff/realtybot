e57a36092d3aa1246f52aa514591dd6f087746d8
Andrew Lavrentev <andrew.lavrentev@gmail.com>
2022-11-13 21:38:39 +0100

    FavoriteButton link

--- app/components/FavoriteButton/FavoriteButton.tsx
+++ app/components/FavoriteButton/FavoriteButton.tsx
@@ -1,13 +1,18 @@
 import { FC } from 'react';
+import { Link } from 'remix';
 import { Text } from '~/components';
+import { routes } from '~/config';
 
 export const FavoriteButton: FC = () => {
   return (
-    <div className="flex w-full flex-row items-center justify-center rounded-lg bg-gray-light p-3">
+    <Link
+      to={routes.favorites}
+      className="flex w-full flex-row items-center justify-center rounded-lg bg-gray-light p-3"
+    >
       <img src={require('./assets/icn-heart.svg')} />
       <Text.Sm color="purple" className="ml-1">
         Избранные объявления
       </Text.Sm>
-    </div>
+    </Link>
   );
 };
