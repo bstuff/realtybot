811635b49496fc66c4b1e529822415eaded77959
Andrew Lavrentev <andrew.lavrentev@gmail.com>
2022-11-13 22:16:16 +0100

    WhatLooking routes

--- app/components/WhatLooking/WhatLooking.tsx
+++ app/components/WhatLooking/WhatLooking.tsx
@@ -1,5 +1,8 @@
 import { FC } from 'react';
+import { Link } from 'remix';
+
 import { Text } from '~/components';
+import { routes } from '~/config';
 
 export const WhatLooking: FC = () => {
   return (
@@ -15,18 +18,21 @@ export const WhatLooking: FC = () => {
       </div>
 
       <div className="flex flex-row gap-3">
-        <div className="flex flex-1 flex-col items-center rounded-2xl bg-[#E1F2FB] p-3 text-center">
+        <Link
+          to={routes.mortgageCalculator}
+          className="flex flex-1 flex-col items-center rounded-2xl bg-[#E1F2FB] p-3 text-center"
+        >
           <img src={require('./assets/icn-calc.svg')} />
           <Text.Xs className="mt-3 inline-block font-semibold">Рассчитать ипотеку</Text.Xs>
-        </div>
-        <div className="flex flex-1 flex-col items-center rounded-2xl bg-[#E7FBE1] p-3 text-center">
+        </Link>
+        <Link to={routes.chooseApartments} className="flex flex-1 flex-col items-center rounded-2xl bg-[#E7FBE1] p-3 text-center">
           <img src={require('./assets/icn-plan.svg')} />
           <Text.Xs className="mt-3 inline-block font-semibold">Подобрать квартиру</Text.Xs>
-        </div>
-        <div className="flex flex-1 flex-col items-center rounded-2xl bg-[#FBE1F4] p-3 text-center">
+        </Link>
+        <Link to={routes.chooseProjects} className="flex flex-1 flex-col items-center rounded-2xl bg-[#FBE1F4] p-3 text-center">
           <img src={require('./assets/icn-bld.svg')} />
           <Text.Xs className="mt-3 inline-block font-semibold">Ознакомиться с проектами</Text.Xs>
-        </div>
+        </Link>
       </div>
     </div>
   );
--- app/config/routes.ts
+++ app/config/routes.ts
@@ -1,3 +1,6 @@
 export const routes = {
   'favorites': '/favorites',
+  'mortgageCalculator': '/mortgage-intro',
+  'chooseApartments': '/project-card',
+  'chooseProjects': '/project-card',
 } as const;
